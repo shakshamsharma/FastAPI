6e2772abe34d463f0d70490d64cd51ba2b0db29a
shakshamsharma <sakshamsharmaa15@gmail.com>
2026-08-04 10:33:13 +0530

    more practice

--- main.py
+++ main.py
@@ -4,4 +4,20 @@ app = FastAPI()
 
 @app.get("/")
 def hello():
-    return {"Hello" : "Saksham"}
+    return {"Name":"Saksham"}
+
+
+#Path Parameter
+@app.get("/country/{country}")
+def country(country):
+    return {"Country": country}
+
+
+#Type Hints
+@app.get("/square/{number}")
+def square(number: int):
+    return {"square": number * number}
+
+#Query Parameters
+
+
