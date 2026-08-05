dc727ca823e4d8ccd52e14863c1333d5ec8b0895
shakshamsharma <sakshamsharmaa15@gmail.com>
2026-08-05 12:23:48 +0530

    more practice

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#http://127.0.0.1:8000/users/saksham
+#uvicorn main:app --reload
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -19,5 +22,57 @@ def square(number: int):
     return {"square": number * number}
 
 #Query Parameters
+
+@app.get("/search")
+def search(q):
+    return {"Search": q}
+
+
+@app.get("/square")
+def square(number: int):
+    return {"Square": number*number}
+
+#Mini Practice
+@app.get("/movie")
+def movie(name):
+    return {"Movie": name}
+
+
+#Question 2
+@app.get("/calculate")
+def calculate(num: int):
+    return {"double": num * 2}
+
+
+#Default Parameter
+@app.get("/Research")
+def Research(q="Python"):
+    return {"Search": q}
+
+@app.get("/catagory")
+def catagory(product="Laptop"):
+    return {"Catagory": product}
+
+
+
+#Multiple Query Parameters
+@app.get("/products")
+def products(catagory, model: int, price: int):
+    return {"Catagory": catagory,
+            "Model": model,
+            "Price": price
+            }
+
+#Optional Query Parameter
+
+@app.get("/catagories")
+def catagories(smartphone, ram=None):
+    return {"SmartPhone": smartphone,
+            "Ram": ram
+        }
+
+
+
+
 
 
